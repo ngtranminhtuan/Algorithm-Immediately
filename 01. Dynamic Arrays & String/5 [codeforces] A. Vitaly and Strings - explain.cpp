eff92d7538b1c6ba99071570fae5aebd6c1f5127
ngtranminhtuan <ngtranminhtuan@gmail.com>
2020-06-11 15:43:53 +0700

    - Vitaly and Strings - command

--- 01. Dynamic Arrays & String/5 [codeforces] A. Vitaly and Strings - explain.cpp
+++ 01. Dynamic Arrays & String/5 [codeforces] A. Vitaly and Strings - explain.cpp
@@ -1,3 +1,4 @@
+/*
 - Chạy ngược chuỗi S từ ký tự cuối về ký tự đầu, xét hai trường hợp sau:
     + Nếu gặp ký tự ‘z’ thì biến ký tự này thành ký tự ‘a’.
     + Nếu gặp ký tự khác ‘z’ thì tăng ký tự này lên một bậc, 
@@ -10,6 +11,7 @@ nếu chuỗi kết quả khác chuỗi T (nghĩa là chuỗi nhỏ hơn T) thì
 kết quả, ngược lại in “No such string”.
 
 Độ phức tạp: O(N) với N là độ dài của 2 chuỗi.
+*/
 
 #include <iostream>
 #include <string>
