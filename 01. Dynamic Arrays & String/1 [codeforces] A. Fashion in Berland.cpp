5e1d95287be511343e5377361b712a930169dc21
Nguyễn Trần Minh Tuấn <ngtranminhtuan@gmail.com>
2021-05-28 18:56:50 +0700

    Update 1 [codeforces] A. Fashion in Berland.cpp

--- 01. Dynamic Arrays & String/1 [codeforces] A. Fashion in Berland.cpp
+++ 01. Dynamic Arrays & String/1 [codeforces] A. Fashion in Berland.cpp
@@ -1,27 +1,66 @@
 // Problem: A. Fashion in Berland
 // Link: http://codeforces.com/contest/691/problem/A
 
-#include <iostream>
-
+#include <iostream>  
+#include <vector>  
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-
-    int x;
-    int nZero = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> x;
-        if (x == 0)
-            nZero++;
-    }
-
+ 
+bool checkJacket(vector<int>& v, int n) {
     if (n == 1) {
-        cout << (nZero == 0 ? "YES" : "NO") << endl;
-    } else {
-        cout << (nZero == 1 ? "YES" : "NO") << endl;
+        if (v[0] == 1)
+            return true;
+        else
+            return false;
     }
-
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == 0) {
+            count++;
+        }
+    }
+    if (count == 1)
+        return true;
+    else
+        return false;
+}
+ 
+int main() {
+    int n, value;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        cin >> value;
+        v.push_back(value);
+    }
+    bool result = checkJacket(v, n);
+    if (result == true)
+        cout << "YES";
+    else
+        cout << "NO";
     return 0;
 }
+
+// Python
+def checkJacket(v, n):
+    if n == 1:
+        if v[0] == 1:
+            return True
+        else:
+            return False
+    count = 0
+    for i in range(n):
+        if v[i] == 0:
+            count += 1
+    if count == 1:
+        return True
+    else:
+        return False
+ 
+ 
+n = int(input())
+v = list(map(int, input().split()))
+ 
+if checkJacket(v, n):
+    print("YES")
+else:
+    print("NO")
